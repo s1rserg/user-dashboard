1b0a6591aa4acc750765f33821fe5ac5342fe250
s1rserg <sirserg05@gmail.com>
2025-12-09 12:37:57 +0200

    feat(ui): add mobile responsiveness

--- components/dashboard/components/UserTable.tsx
+++ components/dashboard/components/UserTable.tsx
@@ -3,57 +3,44 @@ import { FC } from 'react';
 
 interface Props {
   users: User[];
-  headers: {
-    name: string;
-    email: string;
-    company: string;
-  };
+  headers: { name: string; email: string; company: string };
 }
 
 export const UserTable: FC<Props> = ({ users, headers }) => {
   return (
-    <div className="mt-6 flow-root">
-      <div className="inline-block min-w-full align-middle">
-        <div className="overflow-hidden rounded-lg border border-gray-200 shadow-sm overflow-x-auto">
-          <table className="min-w-full divide-y divide-gray-200 bg-white">
-            <thead className="bg-gray-50">
-              <tr>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
-                >
-                  {headers.name}
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
-                >
-                  {headers.email}
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
-                >
-                  {headers.company}
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200 bg-white">
-              {users.map((u) => (
-                <tr key={u.id} className="hover:bg-gray-50 transition-colors">
-                  <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
-                    {u.name}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{u.email}</td>
-                  <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-                    {u.company.name}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <div className="overflow-hidden rounded-lg border border-gray-200">
+      <table className="min-w-full divide-y divide-gray-200 bg-white">
+        <thead className="hidden bg-gray-50 md:table-header-group">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">
+              {headers.name}
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">
+              {headers.email}
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">
+              {headers.company}
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className="divide-y divide-gray-200">
+          {users.map((u) => (
+            <tr
+              key={u.id}
+              className="flex flex-col border-b last:border-none p-2 md:table-row md:p-0 md:border-none hover:bg-gray-50"
+            >
+              <td className="block px-6 py-1 font-medium text-gray-900 md:table-cell md:py-4">
+                {u.name}
+              </td>
+              <td className="block px-6 py-1 text-gray-500 md:table-cell md:py-4">{u.email}</td>
+              <td className="block px-6 py-1 text-gray-500 md:table-cell md:py-4">
+                {u.company.name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
